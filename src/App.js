eb5be6994003fadacc3410e01abd916b6242e1cc
Kanagaraj-R <kanagarajram88@gmail.com>
2023-08-11 22:11:49 +0530

    comiit
    Signed-off-by: Kanagaraj-R <kanagarajram88@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavbarPage from "./Components/navbar";
+import ShopCart from "./Pages/shop/shop";
+import Cart from "./Pages/Cart/cart";
+import "./App.css";
+import ShopContextProvider from "./Context/shop-context";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ShopContextProvider>
+        <Router>
+          <NavbarPage />
+          <Routes>
+            <Route path="/" element={<ShopCart />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
